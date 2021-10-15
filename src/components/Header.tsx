e9a3dad2791cc9d189573c3590a0ec456487e7a9
DinaSem <neispravimayas1@gmail.com>
2021-10-15 21:14:46 +0300

    13 урок

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Header() {
+    return (
+            <header className="header">
+                <img
+                    src="https://e7.pngegg.com/pngimages/197/457/png-clipart-round-multicolored-logo-vanamo-logo-icons-logos-emojis-tech-companies.png"/>
+            </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+function Profile() {
+    return (
+        <div className="content">
+            <img src="https://wallpaperset.com/w/full/f/3/3/476097.jpg" alt=""/>
+            <div>
+                ava+description
+            </div>
+            <div>
+                My Posts
+                <div>
+                    New posts
+                </div>
+                <div>post1</div>
+                <div>post2</div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
