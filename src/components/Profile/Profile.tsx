c9a4e953df755f57487dfbbc05b24b19af22e817
DinaSem <neispravimayas1@gmail.com>
2021-10-18 21:10:14 +0300

    15 урок

---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import s from './Profile.module.css'
+import MyPosts from "./MyPosts/MyPosts";
+
+function Profile() {
+    return (
+        <div className={s.content}>
+            <div>
+                <img src="https://wallpaperset.com/w/full/f/3/3/476097.jpg" alt=""/>
+            </div>
+            <div>
+                ava+description
+            </div>
+            <MyPosts/>
+        </div>)
+}
+    export default Profile;
